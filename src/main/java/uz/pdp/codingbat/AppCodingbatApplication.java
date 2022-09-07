945600aeafc5b61fd991b065c3c9847b276e3c2d
abror <abror01042001@gmail.com>
2022-09-07 19:20:11 +0500

    commit

--- src/main/java/uz/pdp/codingbat/AppCodingbatApplication.java
+++ src/main/java/uz/pdp/codingbat/AppCodingbatApplication.java
@@ -7,6 +7,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class AppCodingbatApplication {
 
     public static void main(String[] args) {
+
+
+
+
         SpringApplication.run(AppCodingbatApplication.class, args);
     }
 
