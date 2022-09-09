08ba0da825c1bd1f8c598b325337819d6660b936
Azizjon <azizabdualiyev6@gmail.com>
2022-09-10 00:48:32 +0500

    role

--- src/main/java/uz/pdp/codingbat/entity/role/Role.java
+++ src/main/java/uz/pdp/codingbat/entity/role/Role.java
@@ -1,5 +1,24 @@
 package uz.pdp.codingbat.entity.role;
 
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.annotations.GenericGenerator;
+import org.springframework.stereotype.Service;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import java.util.UUID;
+
+@Entity
+@Setter
+@Getter
 public class Role {
+
+    @Id
+    @GeneratedValue(generator = "uuid2")
+    @GenericGenerator(name = "uuid2", strategy = "org.hibernate.id.UUIDGenerator")
+    private UUID id;
+    private String name;
 
 }
